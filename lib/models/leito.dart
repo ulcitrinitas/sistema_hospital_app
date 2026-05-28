69f9689e37c23517fb1eb057fc4aa0c24f3310c7
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 19:13:19 -0300

    adicionada herança de model

--- lib/models/leito.dart
+++ lib/models/leito.dart
@@ -1,4 +1,6 @@
-class Leito {
+import 'package:sis_hospital/models/model.dart';
+
+class Leito extends Model {
   int? id;
   String numero;
   String ala;
@@ -12,6 +14,31 @@ class Leito {
     required this.tipo,
     required this.status,
   });
+
+  @override
+  Map<String, dynamic> toJson() {
+    if (id != null) {
+      return {
+        "id": id,
+        "numero": numero,
+        "ala": ala,
+        "tipo": tipo,
+        "status": status,
+      };
+    }
+    return {"numero": numero, "ala": ala, "tipo": tipo, "status": status};
+  }
+
+  @override
+  Leito fromJson(Map<String, dynamic> json) {
+    return Leito(
+      id: parseId(json["id"]),
+      numero: json["numero"] ?? "",
+      ala: json["ala"] ?? "",
+      tipo: json["tipo"] ?? "",
+      status: json["status"] ?? "",
+    );
+  }
 }
 
 enum StatusLeito { ocupado, disponivel }
--- lib/models/prontuario.dart
+++ lib/models/prontuario.dart
@@ -1,9 +1,10 @@
 import 'package:sis_hospital/models/internacao.dart';
 import 'package:sis_hospital/models/medico.dart';
+import 'package:sis_hospital/models/model.dart';
 
-class Prontuario {
+class Prontuario extends Model {
   int? id;
-  DateTime dt_registro;
+  DateTime? dt_registro;
   String descricao;
   String tipo;
 
@@ -18,4 +19,38 @@ class Prontuario {
     required this.medico,
     required this.internacao,
   });
+
+  @override
+  Map<String, dynamic> toJson() {
+    if (id != null) {
+      return {
+        "id": id,
+        "dt_registro": dt_registro,
+        "descricao": descricao,
+        "tipo": tipo,
+        "medico": medico.toJson(),
+        "internacao": internacao.toJson(),
+      };
+    }
+
+    return {
+      "dt_registro": dt_registro,
+      "descricao": descricao,
+      "tipo": tipo,
+      "medico": medico.toJson(),
+      "internacao": internacao.toJson(),
+    };
+  }
+
+  @override
+  Prontuario fromJson(Map<String, dynamic> json) {
+    return Prontuario(
+      id: parseId(json["id"]),
+      dt_registro: parseDateTime(json["dt_registro"]),
+      descricao: json["descricao"] ?? "",
+      tipo: json["tipo"] ?? "",
+      medico: medico.fromJson(json["medico"]),
+      internacao: internacao.fromJson(json["internacao"]),
+    );
+  }
 }
