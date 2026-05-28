cccb313fa89b977a306c4ff157a83a082c4f9c70
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 20:18:09 -0300

    criado controller para consumir a rota medicos

---
+++ lib/controllers/medico.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:sis_hospital/controllers/api.dart';
+import 'package:sis_hospital/models/medico.dart';
+
+import 'package:http/http.dart' as http;
+
+class MedicoController {
+  static final String url = "${ApiController.baseUrl}/medicos";
+
+  static Future<List<Medico>> listarPacientes() async {
+    final respose = await http.get(Uri.parse(url));
+
+    if (respose.statusCode == 200) {
+      final body = respose.body.trim();
+
+      if (body.startsWith("<")) {
+        throw Exception(
+          "A API retornou HTML ao invés de JSON.\n"
+          "Verifique se a API está retornando um json",
+        );
+      }
+
+      final List<dynamic> dados = json.decode(body);
+      return dados.map((json) => Medico.fromJson(json)).toList();
+    }
+    throw Exception("Erro ao listar os pacientes: ${respose.statusCode}");
+  }
+
+  static Future<void> inserirPaciente(Medico paciente) async {
+    final response = await http.post(
+      Uri.parse(ApiController.baseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(paciente.toJson()),
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception(
+        "Erro ao inserir paciente: ${response.statusCode}\n${response.body}",
+      );
+    }
+  }
+
+  static Future<void> atualizarPaciente(Medico paciente) async {
+    final response = await http.put(
+      Uri.parse("${url}?id=${paciente.id}"),
+      headers: {"Content-Type": "application/json"},
+      body: json.encode(paciente.toJson()),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        "Erro ao atualizar paciente: ${response.statusCode}\n${response.body}",
+      );
+    }
+  }
+
+  static Future<void> excluirPaciente(Medico paciente) async {
+    final response = await http.delete(Uri.parse("${url}?id=${paciente.id}"));
+
+    if (response.statusCode != 200) {
+      throw Exception(
+        "Erro ao excluir paciente: ${response.statusCode}\n${response.body}",
+      );
+    }
+  }
+}
