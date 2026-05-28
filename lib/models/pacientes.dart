08a10c8066dbaa4c29c0ce62546a0f889edb81f9
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 01:27:54 -0300

    criado model de pacientes

---
+++ lib/models/pacientes.dart
@@ -0,0 +1,26 @@
+class Pacientes {
+  int? id;
+  String nome;
+  String cpf;
+  String dt_nascimento;
+  String alergias;
+
+  Pacientes({
+    required this.nome,
+    this.id,
+    required this.cpf,
+    required this.dt_nascimento,
+    required this.alergias,
+  });
+
+  DateTime converter_nasc() {
+    var nasc = DateTime.tryParse(dt_nascimento);
+
+    if (nasc == null) {
+      print("Erro! Problema ao formatar a data");
+      return DateTime.now();
+    } else {
+      return nasc;
+    }
+  }
+}
