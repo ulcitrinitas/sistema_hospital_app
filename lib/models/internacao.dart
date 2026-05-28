50457d997c302e7a66eedeb156191ad5a964c6ee
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 13:22:18 -0300

    adicionado construtor da internação

--- lib/models/internacao.dart
+++ lib/models/internacao.dart
@@ -1,7 +1,20 @@
+import 'paciente.dart';
+import 'medico.dart';
+import 'leito.dart';
+
 class Internacao {
+  int? id;
+  DateTime dt_entrada;
 
+  Paciente paciente;
+  Medico medico;
+  Leito leito;
 
-  
-
-
+  Internacao({
+    this.id,
+    required this.dt_entrada,
+    required this.paciente,
+    required this.medico,
+    required this.leito,
+  });
 }
