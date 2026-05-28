628fe24a1fb424d56a82b561f38c9ba11837d777
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 18:56:45 -0300

    criadas funções de converter os dados

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -3,10 +3,22 @@ import 'package:intl/intl.dart';
 abstract class Model {
   Map<String, dynamic> toJson();
 
-  DateTime? parseDateTimeIntl(String input, {String format = 'yyyy-MM-dd'}) {
+  Model fromJson(Map<String, dynamic> json);
+
+  DateTime? parseDateTime(String input, {String format = 'yyyy-MM-dd'}) {
     try {
       return DateFormat(format).parse(input);
     } catch (_) {
+      return null;
+    }
+  }
+
+  int? parseId(dynamic id) {
+    var idConverted = int.tryParse(id.toString());
+
+    if (idConverted != null) {
+      return idConverted;
+    } else {
       return null;
     }
   }
