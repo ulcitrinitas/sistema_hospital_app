900806bf7d1805b371a6a2c098dbadf401bf3dd2
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 19:13:35 -0300

    adicionada herança de model

--- lib/models/internacao.dart
+++ lib/models/internacao.dart
@@ -1,10 +1,12 @@
+import 'package:sis_hospital/models/model.dart';
+
 import 'paciente.dart';
 import 'medico.dart';
 import 'leito.dart';
 
-class Internacao {
+class Internacao extends Model {
   int? id;
-  DateTime dt_entrada;
+  DateTime? dt_entrada;
 
   Paciente paciente;
   Medico medico;
@@ -17,4 +19,35 @@ class Internacao {
     required this.medico,
     required this.leito,
   });
+
+  @override
+  Map<String, dynamic> toJson() {
+    if (id != null) {
+      return {
+        "id": id,
+        "dt_entrada": dt_entrada,
+        "paciente": paciente.toJson(),
+        "medico": medico.toJson(),
+        "leito": leito.toJson(),
+      };
+    }
+
+    return {
+      "dt_entrada": dt_entrada,
+      "paciente": paciente.toJson(),
+      "medico": medico.toJson(),
+      "leito": leito.toJson(),
+    };
+  }
+
+  @override
+  Internacao fromJson(Map<String, dynamic> json) {
+    return Internacao(
+      id: parseId(json["id"]),
+      dt_entrada: parseDateTime(json["dt_entrada"]),
+      paciente: paciente.fromJson(json["paciente"]),
+      medico: medico.fromJson(json["medico"]),
+      leito: leito.fromJson(json["leito"]),
+    );
+  }
 }
