5ea3b99b22215bf75f92d2229f5ce848ed320c2f
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 01:34:17 -0300

    cirda classe leitos

---
+++ lib/models/leitos.dart
@@ -0,0 +1,15 @@
+class Leitos {
+  int? id;
+  String numero;
+  String ala;
+  String tipo;
+  String status;
+
+  Leitos({
+    this.id,
+    required this.numero,
+    required this.ala,
+    required this.tipo,
+    required this.status,
+  });
+}
