2f9f1b8bcdafd75f4616e730c0ce9f29656fd5f0
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-28 01:30:48 -0300

    adicionado contrutor

--- lib/models/medicos.dart
+++ lib/models/medicos.dart
@@ -1,3 +1,13 @@
 class Medicos {
-  
+  int? id;
+  String nome;
+  String crm;
+  String especialidade;
+
+  Medicos({
+    this.id,
+    required this.nome,
+    required this.crm,
+    required this.especialidade,
+  });
 }
