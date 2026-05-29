316df2ac660a5cb79abb9e38003d33651d479ac5
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-29 12:17:26 -0300

    criada classe para a tela de listar pacientes

---
+++ lib/screens/pacientes/listar_pacientes_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListarPacientes extends StatefulWidget {
+  const ListarPacientes({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _ListarPacientesState();
+
+}
+
+class _ListarPacientesState extends State<ListarPacientes> {
+  
+}
+
