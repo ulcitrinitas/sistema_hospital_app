1b0dab8c0df2440bdfba7d23d3f018bd6c15a822
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-29 18:51:55 -0300

    renomeada classe

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScren extends StatelessWidget {
-  const HomeScren({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
