8638bf8087b7cf31bd8f49619db61c7e3fda66d0
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-29 19:06:37 -0300

    criado tela home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,89 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Sistema do Hospital"),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Bem-vindo",
-              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  _HomeCard(
-                    icon: Icons.person,
-                    label: "Pacientes",
-                    color: Colors.blue,
-                    onTap: () => Navigator.pushNamed(context, "/pacientes"),
-                  ),
-                  _HomeCard(
-                    icon: Icons.medical_services,
-                    label: "Médicos",
-                    color: Colors.teal,
-                    onTap: () => Navigator.pushNamed(context, "/medicos"),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  State<StatefulWidget> createState() => _HomeScreenState();
 }
 
-class _HomeCard extends StatelessWidget {
-  final IconData icon;
-  final String label;
-  final Color color;
-  final VoidCallback onTap;
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
 
-  _HomeCard({
-    required this.icon,
-    required this.label,
-    required this.color,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          border: Border.all(),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 48, color: color),
-            const SizedBox(height: 12),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-                color: color,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  final List<Widget> _abas = [const HomeDashboard()];
 }
