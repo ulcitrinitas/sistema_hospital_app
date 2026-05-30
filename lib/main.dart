080a46b17d3fbe37947326c13c9dd37f45d189af
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-29 23:04:45 -0300

    definido configurações iniciais do app

--- lib/main.dart
+++ lib/main.dart
@@ -13,41 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Sistema do Hospital",
       debugShowCheckedModeBanner: true,
-      initialRoute: "/home",
-      routes: {"/home": (context) => HomeScreen()},
-    );
-  }
-}
-
-class PlaceholderScreen extends StatelessWidget {
-  final String title;
-  const PlaceholderScreen({super.key, required this.title});
-
-  int _indexFor(String title) {
-    switch (title) {
-      case "Leitos":
-        return 1;
-      case "Internações":
-        return 2;
-      case "Prontuários":
-        return 3;
-      default:
-        return 0;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(this.title)),
-      body: Center(
-        child: Text(
-          "${this.title}\n(em breve)",
-          textAlign: TextAlign.center,
-          style: const TextStyle(fontSize: 20, color: Colors.grey),
-        ),
-      ),
-      bottomNavigationBar: AppBottomNavBar(currentIndex: _indexFor(this.title)),
+      theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      home: const HomeScreen(),
     );
   }
 }
