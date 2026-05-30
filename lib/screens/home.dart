0eae76df6803d5f98223cbe12d4962ef68e583a3
Ulisses Silva <ulisses.scsilva@proton.me>
2026-05-29 23:04:15 -0300

    criada tela principal com os seletores

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sis_hospital/screens/home_dashboard.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,5 +11,64 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 0;
 
-  final List<Widget> _abas = [const HomeDashboard()];
+  final List<Widget> _abas = [
+    HomeDashboard(),
+    Center(child: Text("Leitos\n(Em breve)", textAlign: TextAlign.center)),
+    Center(child: Text("Internações\n(Em breve)", textAlign: TextAlign.center)),
+    Center(child: Text("Prontuários\n(Em breve)", textAlign: TextAlign.center)),
+  ];
+
+  void _tapIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _abas[_currentIndex],
+      drawer: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            child: Text("Sistema do Hospital"),
+            decoration: BoxDecoration(color: Colors.blue),
+          ),
+          ListTile(
+            title: Text("Página Principal"),
+            selected: _currentIndex == 0,
+            onTap: () {
+              _tapIndex(0);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text("Leitos"),
+            selected: _currentIndex == 1,
+            onTap: () {
+              _tapIndex(1);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text("Internações"),
+            selected: _currentIndex == 2,
+            onTap: () {
+              _tapIndex(2);
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            title: Text("Prontuários"),
+            selected: _currentIndex == 3,
+            onTap: () {
+              _tapIndex(3);
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
